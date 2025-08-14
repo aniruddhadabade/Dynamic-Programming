442d0842be16a2489d1b14e0870f02f92c2fbed6
aniruddhadabade <aniruddhadabade123@gmail.com>
2025-08-14 21:04:09 +0530

    //

---
+++ LCS/printingLcs.java
@@ -0,0 +1,50 @@
+package LCS;
+
+public class printingLcs {
+    public String printlongestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+
+        int dp[][] = new int[m+1][n+1];
+
+        for(int i=0; i<m+1; i++){
+            for(int j=0; j<n+1; j++){
+                if(i==0 || j==0){
+                    dp[i][j] = 0;
+                }
+
+                if(i>0 && j>0){
+                    if(text1.charAt(i-1)==text2.charAt(j-1)){
+                        dp[i][j] = 1 + dp[i-1][j-1];
+                    }
+                    else{
+                        dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                    }
+                }
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        int i=m;
+        int j=n;
+
+        while (i>0 && j>0) {
+            if(text1.charAt(i-1)==text2.charAt(j-1)){
+                i--;
+                j--;
+            }
+            else{
+                if(dp[i-1][j]>dp[i][j-1]){
+                    i--;
+                }
+                else{
+                    j--;
+                }
+            }
+        }
+        return sb.reverse().toString();
+    }
+    public static void main(String[] args) {
+        
+    }
+}
